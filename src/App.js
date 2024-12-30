8bb2ca1d90e8d67fac1e64e45ca4318f2a3a5e34
sumancfc <sumanstha999@gmail.com>
2024-12-30 11:42:57 +0100

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import Form from './Form';
-import Recipe from './Recipe';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Form from "./Form";
+import Recipe from "./Recipe";
+import "./App.css";
 
 function App() {
-  const APP_ID = '0617c1cf';
-  const APP_KEY = '29ffc30c48221cb7121fb6442270d38f';
+  const appID = process.env.REACT_APP_API_ID;
+  const appKey = process.env.REACT_APP_API_KEY;
 
   const [recipes, setRecipes] = useState([]);
-  const [text, setText] = useState('');
-  const [query, setQuery] = useState('chcicken');
-  // const
+  const [text, setText] = useState("");
+  const [query, setQuery] = useState("chcicken");
 
   useEffect(() => {
     const getTheRecipe = async () => {
       const response = await fetch(
-        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+        `https://api.edamam.com/search?q=${query}&app_id=${appID}&app_key=${appKey}`
       );
       const data = await response.json();
       console.log(data.hits);
@@ -32,7 +31,7 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setQuery(text);
-    setText('');
+    setText("");
   };
 
   return (
